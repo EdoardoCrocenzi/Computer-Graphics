ec6954ba68dcf76b5c33e6253afd373d0755c0ca
DragonNest <71124432+EdoardoCrocenzi@users.noreply.github.com>
2022-05-15 13:02:25 +0200

    Add files via upload

---
+++ 02_raytrace_out/out/run.cmd
@@ -0,0 +1,20 @@
+.\bin\yraytrace --scene tests\02_matte\matte.json --output out\lowres\0x_color_720_9.jpg --samples 9 --shader color --resolution 720
+.\bin\yraytrace --scene tests\02_matte\matte.json --output out\lowres\0x_normal_720_9.jpg --samples 9 --shader normal --resolution 720
+.\bin\yraytrace --scene tests\02_matte\matte.json --output out\lowres\0x_texcoord_720_9.jpg --samples 9 --shader texcoord --resolution 720
+.\bin\yraytrace --scene tests\02_matte\matte.json --output out\lowres\0x_eyelight_720_9.jpg --samples 9 --shader eyelight --resolution 720
+
+.\bin\yraytrace --scene tests\00_refractive\refractive.json --output out\lowres\00_refractive.jpg --samples 256 --resolution 512
+.\bin\yraytrace --shader cellshading --scene tests\001_cellshading\cellshading.json --output out\lowres\001_cellshading.jpg --samples 256 --resolution 512
+.\bin\yraytrace --shader matcap --scene tests\000_matcap\matcap.json --output out\lowres\000_matcap.jpg --samples 256 --resolution 512
+.\bin\yraytrace --scene tests\01_cornellbox\cornellbox.json --output out\lowres\01_cornellbox_512_256.jpg --samples 256 --resolution 512
+.\bin\yraytrace --scene tests\02_matte\matte.json --output out\lowres\02_matte_720_256.jpg --samples 25 --resolution 720
+.\bin\yraytrace --scene tests\03_texture\texture.json --output out\lowres\03_texture_720_256.jpg --samples 256 --resolution 720
+.\bin\yraytrace --scene tests\04_envlight\envlight.json --output out\lowres\04_envlight_720_256.jpg --samples 256 --resolution 720
+.\bin\yraytrace --scene tests\05_arealight\arealight.json --output out\lowres\05_arealight_720_256.jpg --samples 256 --resolution 720
+.\bin\yraytrace --scene tests\06_metal\metal.json --output out\lowres\06_metal_720_256.jpg --samples 256 --resolution 720
+.\bin\yraytrace --scene tests\07_plastic\plastic.json --output out\lowres\07_plastic_720_256.jpg --samples 256 --resolution 720
+.\bin\yraytrace --scene tests\08_glass\glass.json --output out\lowres\08_glass_720_256.jpg --samples 256 --bounces  8 --resolution 720
+.\bin\yraytrace --scene tests\09_opacity\opacity.json --output out\lowres\09_opacity_720_256.jpg --samples 256 --resolution 720
+.\bin\yraytrace --scene tests\10_hair\hair.json --output out\lowres\10_hair_720_256.jpg --samples 256 --resolution 720
+.\bin\yraytrace --scene tests\11_bathroom1\bathroom1.json --output out\lowres\11_bathroom1_720_256.jpg --samples 256 --bounces  8 --resolution 720
+.\bin\yraytrace --scene tests\12_ecosys\ecosys.json --output out\lowres\12_ecosys_720_256.jpg --samples 256 --resolution 720
